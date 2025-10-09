01083f1cc88ab861d13c5bea90995fcfc92e11bc
Joshua <joshuajulyus27@gmail.com>
2025-10-09 23:07:26 +0700

    implement register

--- my-app/src/app/register/page.tsx
+++ my-app/src/app/register/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { showError } from "@/helpers/alert"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
+
 export default function LoginPage() {
+    const router = useRouter()
     const [forms, setForm] = useState({
         email: '',
         username: '',
@@ -18,62 +22,86 @@ export default function LoginPage() {
         }))
     }
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const resp = await fetch('http://localhost:3000/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(forms)
+            })
+            const data = await resp.json()
+            if (!resp.ok) {
+                showError(data.message)
+            }
+            router.push('/login')
+        } catch (error) {
+            console.log(error, '<<< error register page')
+            showError(error)
+        }
+
+    }
+
     const isFilled = forms.email.trim().length > 0 || forms.password.trim().length > 0
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-base-100">
             <div className="fieldset bg-base-200 border-base-300 rounded-box border p-8 w-full max-w-lg">
                 <div className="text-3xl font-bold text-center mb-6">Register</div>
+                <form onSubmit={handleSubmit}>
+                    <label className="label">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        className="input input-bordered w-full"
+                        placeholder="Email"
+                        onChange={handleChange}
+                        value={forms.email}
+                    />
 
-                <label className="label">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    className="input input-bordered w-full"
-                    placeholder="Email"
-                    onChange={handleChange}
-                    value={forms.email}
-                />
+                    <label className="label">Username</label>
+                    <input
+                        type="username"
+                        name="username"
+                        className="input input-bordered w-full"
+                        placeholder="Username"
+                        onChange={handleChange}
+                        value={forms.username}
+                    />
 
-                <label className="label">Username</label>
-                <input
-                    type="username"
-                    name="username"
-                    className="input input-bordered w-full"
-                    placeholder="Username"
-                    onChange={handleChange}
-                    value={forms.username}
-                />
+                    <label className="label">Name</label>
+                    <input
+                        type="name"
+                        name="name"
+                        className="input input-bordered w-full"
+                        placeholder="Name"
+                        onChange={handleChange}
+                        value={forms.name}
+                    />
 
-                <label className="label">Name</label>
-                <input
-                    type="name"
-                    name="name"
-                    className="input input-bordered w-full"
-                    placeholder="Name"
-                    onChange={handleChange}
-                    value={forms.name}
-                />
+                    <label className="label mt-4">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="input input-bordered w-full"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        value={forms.password}
+                    />
 
-                <label className="label mt-4">Password</label>
-                <input 
-                type="password" 
-                name="password"
-                className="input input-bordered w-full" 
-                placeholder="Password" 
-                onChange={handleChange}
-                value={forms.password}
-                />
-
-                <button
-                    disabled={!isFilled}
-                    className={`w-full mt-6 py-3 rounded-full font-semibold transition-colors duration-300 ${isFilled
+                    <button
+                        type="submit"
+                        disabled={!isFilled}
+                        className={`cursor-pointer w-full mt-6 py-3 rounded-full font-semibold transition-colors duration-300 ${isFilled
                             ? "bg-[#83b81a] text-white hover:bg-[#6fa015]"
                             : "bg-[#cde7b1] text-gray-400 cursor-not-allowed"
-                        }`}
-                >
-                    Login
-                </button>
+                            }`}
+                    >
+                        Login
+                    </button>
+                </form>
             </div>
         </div>
     )
