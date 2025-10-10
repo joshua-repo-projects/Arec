51d43d57fda77d983acabb674280e859c4fe7420
Joshua <joshuajulyus27@gmail.com>
2025-10-10 07:20:47 +0700

    implement login within middleware

---
+++ my-app/src/app/login/action.ts
@@ -0,0 +1,8 @@
+'use server'
+
+import { cookies } from "next/headers"
+
+export async function setCookie(key: string, value:string): Promise<void> {
+    const cookieStore = await cookies()
+    cookieStore.set(key, value)
+}
--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
+import { showError } from "@/helpers/alert"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { setCookie } from "./action"
 
 export default function LoginPage() {
     const [forms, setForm] = useState({
         email: '',
         password: ''
     })
+    const [loading, setLoading] = useState(false)
+    const router = useRouter()
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
@@ -16,6 +21,33 @@ export default function LoginPage() {
         }))
     }
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            const resp = await fetch('http://localhost:3000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(forms)
+            })
+            const data = await resp.json()
+
+            if (!resp.ok) {
+                showError(data.message)
+                return
+            }
+            await setCookie('access_token', data.token)
+            router.push('/products')
+        } catch (error: unknown) {
+            console.log(error, '<<< error login page')
+            showError(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     const isFilled = forms.email.trim().length > 0 || forms.password.trim().length > 0
 
     return (
@@ -23,35 +55,38 @@ export default function LoginPage() {
             <div className="fieldset bg-base-200 border-base-300 rounded-box border p-8 w-full max-w-lg">
                 <div className="text-3xl font-bold text-center mb-6">Login</div>
 
-                <label className="label">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    className="input input-bordered w-full"
-                    placeholder="Email"
-                    onChange={handleChange}
-                    value={forms.email}
-                />
+                <form onSubmit={handleSubmit}>
+                    <label className="label">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        className="input input-bordered w-full"
+                        placeholder="Email"
+                        onChange={handleChange}
+                        value={forms.email}
+                    />
 
-                <label className="label mt-4">Password</label>
-                <input 
-                type="password" 
-                name="password"
-                className="input input-bordered w-full" 
-                placeholder="Password" 
-                onChange={handleChange}
-                value={forms.password}
-                />
+                    <label className="label mt-4">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="input input-bordered w-full"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        value={forms.password}
+                    />
 
-                <button
-                    disabled={!isFilled}
-                    className={`w-full mt-6 py-3 rounded-full font-semibold transition-colors duration-300 ${isFilled
+                    <button
+                        type="submit"
+                        disabled={!isFilled}
+                        className={`w-full mt-6 py-3 rounded-full font-semibold transition-colors duration-300 ${isFilled
                             ? "bg-[#83b81a] text-white hover:bg-[#6fa015]"
                             : "bg-[#cde7b1] text-gray-400 cursor-not-allowed"
-                        }`}
-                >
-                    Login
-                </button>
+                            }`}
+                    >
+                        Login
+                    </button>
+                </form>
             </div>
         </div>
     )
