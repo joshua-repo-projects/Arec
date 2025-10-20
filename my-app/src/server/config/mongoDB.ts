3d685ed1e5045711ef742a05211e3a4d824960cc
Joshua <joshuajulyus27@gmail.com>
2025-10-20 17:52:00 +0700

    fix mongodb config

--- my-app/src/server/config/mongoDB.ts
+++ my-app/src/server/config/mongoDB.ts
@@ -1,6 +1,6 @@
 import { Db, MongoClient } from "mongodb";
 
-const uri: string = 'mongodb+srv://mongodb:mongodb@cluster0.ovmxxzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const uri: string = process.env.MONGODB_URI as string
 const client = new MongoClient(uri)
 
 let db: Db
